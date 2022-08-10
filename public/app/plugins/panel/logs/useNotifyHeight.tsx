5079777e9508d8466870622d8a349dbfd772f1af
MikeFuster <85583835+MikeFuster@users.noreply.github.com>
2022-08-10 19:05:24 -0300

    Reverting the logs post message logic (#28)
    * change logic
    
    * revert changes
    
    * add check
    
    * bump v

--- public/app/plugins/panel/logs/useNotifyHeight.tsx
+++ public/app/plugins/panel/logs/useNotifyHeight.tsx
@@ -3,6 +3,10 @@ import { useEffect } from 'react';
 const useNotifyHeight = () => {
   useEffect(() => {
     const interval = setInterval(() => {
+      if (self === top) {
+        return;
+      }
+
       const scrollbar = document.querySelector('.scrollbar-view');
       const panelTitle = document.querySelector('.panel-title');
       if (scrollbar && panelTitle) {
