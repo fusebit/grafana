b140e8cec5aa7e3cc094d2ea4c6bdb97cc7cb6c1
Martin Fuster <mfuster@litebox.ai>
2022-08-24 15:09:53 -0300

    add retro-compatibility

--- packages/grafana-ui/src/utils/disableUi.ts
+++ packages/grafana-ui/src/utils/disableUi.ts
@@ -1,11 +1,19 @@
 export const getIsUIHidden = (uiElement: string) => {
   const urlParams = new URLSearchParams(window.location.search);
   const hiddenUiElements = urlParams.get('hideUi')?.split(',');
-  let isUiElementHidden = false;
-  hiddenUiElements?.forEach((element) => {
-    if (!isUiElementHidden) {
-      isUiElementHidden = element === uiElement;
-    }
-  });
+  const isUiElementHidden = Number(hiddenUiElements?.indexOf(uiElement)) > -1;
+
+  // LEGACY SUPPORT - TODO: Remove once the ui gets hidden using hideUi
+  const disablePanelTitle = urlParams.get('disablePanelTitle') === 'true';
+  const disableBreadcrumbs = typeof urlParams.get('disableBreadcrumbs') === 'string';
+  const disableTvButton = typeof urlParams.get('disableTvButton') === 'string';
+  if (!isUiElementHidden && uiElement === 'panelTitle') {
+    return disablePanelTitle;
+  } else if (!isUiElementHidden && uiElement === 'breadcrumbs') {
+    return disableBreadcrumbs;
+  } else if (!isUiElementHidden && uiElement === 'tvButton') {
+    return disableTvButton;
+  }
+
   return isUiElementHidden;
 };
